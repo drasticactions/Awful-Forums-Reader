4f5c3d9bcb599288a71a0e73d35cb43eb0ebfb96
Tim Miller <innerlogic4321@gmail.com>
2015-02-02 22:29:08 -0500

    Actually add last page command

--- AwfulForumsReader/AwfulForumsReader.Shared/Commands/LastPageCommand.cs
+++ AwfulForumsReader/AwfulForumsReader.Shared/Commands/LastPageCommand.cs
@@ -5,20 +5,31 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using AwfulForumsReader.Common;
 using AwfulForumsLibrary.Entity;
+using AwfulForumsReader.Database.Commands;
+using AwfulForumsReader.Pages;
+using AwfulForumsReader.Tools;
 
 namespace AwfulForumsReader.Commands
 {
     public class LastPageCommand : AlwaysExecutableCommand
     {
-        public override void Execute(object parameter)
+        public async override void Execute(object parameter)
         {
             var thread = parameter as ForumThreadEntity;
             if (thread == null)
                 return;
             thread.CurrentPage = thread.TotalPages;
-            //string jsonObjectString = JsonConvert.SerializeObject(thread);
-            //var rootFrame = Window.Current.Content as Frame;
-            //if (rootFrame != null) rootFrame.Navigate(typeof(ThreadPage), jsonObjectString);
+            thread.RepliesSinceLastOpened = 0;
+            App.RootFrame.Navigate(typeof(ThreadPage));
+            var tabManager = new TabManager();
+            await tabManager.RemoveAllThreadsFromTabList();
+            await tabManager.AddThreadToTabListAsync(thread);
+            var tabThreads = await tabManager.GetAllTabThreads();
+            Locator.ViewModels.ThreadPageVm.LinkedThreads = tabThreads.ToObservableCollection();
+
+            Locator.ViewModels.ThreadPageVm.ForumThreadEntity = thread;
+            Locator.ViewModels.ThreadPageVm.Html = null;
+            await Locator.ViewModels.ThreadPageVm.GetForumPostsAsync();
         }
     }
 }
