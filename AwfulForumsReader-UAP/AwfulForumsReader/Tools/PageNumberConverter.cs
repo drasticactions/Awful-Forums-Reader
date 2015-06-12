da9e24b062f410285c83cacec87331580e3c2841
Timothy Miller <innerlogic4321@gmail.com>
2015-06-12 09:46:11 -0400

    Add PageNumberConverter

---
+++ AwfulForumsReader-UAP/AwfulForumsReader/Tools/PageNumberConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace AwfulForumsReader.Tools
+{
+    public class PageNumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var currentPage =(int)value;
+            return $"{currentPage} / {Locator.ViewModels.ThreadPageVm.ForumThreadEntity.TotalPages}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
