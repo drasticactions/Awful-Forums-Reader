b2be29899a0d528e2c9b815c66c85c98009bd6f8
Tim Miller <innerlogic4321@gmail.com>
2015-07-31 07:45:17 -0400

    Make RepliesSinceLastOpenedColorConverter color darker

--- AwfulForumsReader/Tools/RepliesSinceLastOpenedColorConverter.cs
+++ AwfulForumsReader/Tools/RepliesSinceLastOpenedColorConverter.cs
@@ -13,7 +13,7 @@ namespace AwfulForumsReader.Tools
         {
             if (value == null) return new SolidColorBrush(Colors.Black);
             var count = (int)value;
-            return count > 0 ? new SolidColorBrush(Colors.Black) : new SolidColorBrush(Colors.LightGray);
+            return count > 0 ? new SolidColorBrush(Colors.Black) : new SolidColorBrush(Color.FromArgb(255, 131, 131, 131));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
